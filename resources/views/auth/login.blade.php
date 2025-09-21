1c494c2f03256960719cb4f2d0810139adc5ff55
abdulohmuh2005 <abdulohmuh2005@gmail.com>
2025-09-21 19:04:31 +0600

    create registration authorization

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<form method="POST" action="{{ route('login') }}">
+    @csrf
+    <input type="email" name="email" placeholder="Email" required autofocus>
+    <input type="password" name="password" placeholder="Пароль" required>
+    <button type="submit">Войти</button>
+</form>
+
+@if ($errors->any())
+    <div style="color: red">
+        @foreach ($errors->all() as $error)
+            {{ $error }}
+        @endforeach
+    </div>
+@endif
+
+<a href="{{ route('register') }}">Зарегистрироваться</a>
+@endsection
